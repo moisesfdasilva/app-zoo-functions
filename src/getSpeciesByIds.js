d56a9cf53d0b11a248ffdbf616154339ca8c10f8
moisesfdasilva <msesfernandes@gmail.com>
2022-07-28 18:04:22 -0300

    Realizado ajustes no primeiro requisito, em getSpeciesByIds.js

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,14 +2,14 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const reg = [...ids];
-  const result = [];
+  let result1 = [];
+  let result2 = [];
+
   for (let index = 0; index < reg.length; index += 1) {
-    result[index] = (data.species.filter((a) => a.id === reg[index]));
+    result1[index] = (data.species.filter((a) => a.id === reg[index]));
+    result2[index] = result1[index][0];
   }
-  return result;
+  return result2;
 }
-// const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
-// const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
-// console.log(getSpeciesByIds(lionId, tigersId));
 
 module.exports = getSpeciesByIds;
