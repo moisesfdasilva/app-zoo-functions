1247e4c6a170e470e3ccbe9e870f14ac767eeaeb
moisesfdasilva <msesfernandes@gmail.com>
2022-07-28 18:20:38 -0300

    Realizado ajustes no primeiro requisito conforme o linter, em getSpeciesByIds.js

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,12 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const reg = [...ids];
-  let result1 = [];
-  let result2 = [];
+  const result1 = [];
+  const result2 = [];
 
   for (let index = 0; index < reg.length; index += 1) {
     result1[index] = (data.species.filter((a) => a.id === reg[index]));
-    result2[index] = result1[index][0];
+    result2.push(result1[index][0]);
   }
   return result2;
 }
