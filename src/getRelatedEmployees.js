b7b2f126d1e7e744266675a5a622838b02d94963
moisesfdasilva <msesfernandes@gmail.com>
2022-07-29 12:43:06 -0300

    Realizado ajustes (retirada a estrutura Try/Catch) no quarto requisito, em getRelatedEmployees.js

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -17,24 +17,20 @@ const verifyId = (managerId) => {
 };
 
 function getRelatedEmployees(managerId) {
-  try {
-    verifyId(managerId);
-    const empArrId = [];
-    data.employees.forEach((element1) => (
-      element1.managers.forEach((element2) => (
-        empArrId.push({
-          emp: `${element1.firstName} ${element1.lastName}`,
-          man: element2,
-        })
-      ))
-    ));
-    const separateEmployee = (empArrId.filter((object) => object.man === managerId));
-    const empArrName = [];
-    separateEmployee.forEach((element) => (empArrName.push(element.emp)));
-    return empArrName;
-  } catch (error) {
-    return error.message;
-  }
+  verifyId(managerId);
+  const empArrId = [];
+  data.employees.forEach((element1) => (
+    element1.managers.forEach((element2) => (
+      empArrId.push({
+        emp: `${element1.firstName} ${element1.lastName}`,
+        man: element2,
+      })
+    ))
+  ));
+  const separateEmployee = (empArrId.filter((object) => object.man === managerId));
+  const empArrName = [];
+  separateEmployee.forEach((element) => (empArrName.push(element.emp)));
+  return empArrName;
 }
 
 console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
